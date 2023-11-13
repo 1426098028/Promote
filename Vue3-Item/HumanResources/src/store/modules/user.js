84da3b271f375c60ced39d72c8d6b379b909b086
15622081255 <15622081255@gmail.com>
2023-11-13 17:57:34 +0800

    完成登录Token数据持久化

--- Vue3-Item/HumanResources/src/store/modules/user.js
+++ Vue3-Item/HumanResources/src/store/modules/user.js
@@ -1,9 +1,16 @@
-const state = { token: null }
+import { getToken, setToken, removeToken } from '@/utils/auth'
+
+const state = { token: getToken() }
 
 const mutations = {
   setToken(state, token) {
     console.log(state, token)
     state.token = token
+    setToken(token)
+  },
+  setRemoveToken(state, token) {
+    state.token = null
+    removeToken()
   }
 }
 
