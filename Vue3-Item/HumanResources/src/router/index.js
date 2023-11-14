29b2d6629c49f52cb2b77b40df5353d46485f353
15622081255 <15622081255@gmail.com>
2023-11-14 21:26:43 +0800

    调整项目Logo和创建页面路由

--- Vue3-Item/HumanResources/src/router/index.js
+++ Vue3-Item/HumanResources/src/router/index.js
@@ -5,7 +5,14 @@ Vue.use(Router)
 
 /* Layout */
 import Layout from '@/layout'
-
+import departmentRouter from './modules/department'
+import approvalRouter from './modules/approval'
+import attendanceRouter from './modules/attendance'
+import employeeRouter from './modules/employee'
+import permissionRouter from './modules/permission'
+import roleRouter from './modules/role'
+import salaryRouter from './modules/salary'
+import socialRouter from './modules/social'
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
@@ -54,6 +61,14 @@ export const constantRoutes = [
       meta: { title: 'Dashboard', icon: 'dashboard' }
     }]
   },
+  departmentRouter,
+  roleRouter,
+  employeeRouter,
+  permissionRouter,
+  attendanceRouter,
+  approvalRouter,
+  salaryRouter,
+  socialRouter,
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
 ]
