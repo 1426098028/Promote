b6a257fea216612c052c588869c0b6635edbb539
15622081255 <15622081255@gmail.com>
2024-09-12 13:39:54 +0800

    调整vue-router和增加全局路由守卫

---
+++ Electron/011.项目/Electron-Vue3-Ts/src/renderer/src/router/guard.ts
@@ -0,0 +1,19 @@
+// 全局前置守卫
+const beforeEach = (to, from) => {
+    console.log('全局前置守卫');
+    // 返回 false 以取消当次跳转
+    return true;
+};
+// 全局解析守卫
+const beforeResolve = (to, from) => {
+    console.log('全局解析守卫');
+    // 返回 false 以取消当次跳转，
+    return true;
+};
+
+// 全局后置钩子 
+const afterEach = (to, from) => {
+    console.log('全局后置钩子');
+};
+
+export { beforeEach, beforeResolve, afterEach };
--- Electron/011.项目/Electron-Vue3-Ts/src/renderer/src/router/index.ts
+++ Electron/011.项目/Electron-Vue3-Ts/src/renderer/src/router/index.ts
@@ -1,12 +1,17 @@
 import { createRouter, createMemoryHistory } from 'vue-router';
-import Home from '@/views/Home.vue';
-const routes = [
-    {
-        path: '/', component: Home
-    }
-];
+import { beforeEach, beforeResolve, afterEach } from '@/router/guard';
+import { AppRoutes } from '@/router/routes';
+
 const router = createRouter({
     history: createMemoryHistory(),
-    routes
+    routes: AppRoutes
 });
-export { router };
+
+// 全局前置守卫
+router.beforeEach(beforeEach);
+// 全局解析守卫
+router.beforeResolve(beforeResolve);
+// 全局后置钩子 
+router.afterEach(afterEach);
+
+export { router, AppRoutes };
